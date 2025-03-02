24028798a905a8b36089d6e1866d23d96ce07351
Bilal Ahmad <bilal.akbar@likeminded.care>
2025-03-02 14:20:25 +0100

    complete point 1 add basic style

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,9 +3,32 @@ import React, { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+    <div
+      style={{
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "row",
+        gap: "15px",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h2>Current count {count}</h2>
+      <button
+        onClick={() => setCount(count + 1)}
+        style={{
+          padding: "10px",
+          fontSize: "15px",
+          fontWeight: "bold",
+          cursor: "pointer",
+          borderRadius: "5px",
+          backgroundColor: "lightgray",
+          border: "none",
+        }}
+      >
+        +1
+      </button>
     </div>
   );
 };
